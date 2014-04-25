49b2fad30e0125c98be4736804c3816f420f929b
Aradea Krisnaraga <aradea.aks@gmail.com>
2014-04-26 00:38:21 +0700

    GUI udh kelar, tinggal ngolah input :D

---
+++ src/rc4kripto/DekripGUI.java
@@ -0,0 +1,121 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rc4kripto;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Aradea
+ */
+public class DekripGUI {	
+	JPanel menu;
+	GroupLayout layout;
+	JButton plaintext;
+	JButton key;
+	JButton encrypt;
+	JButton save;
+	JTextField pathPlain;
+	JTextField pathKey;
+	JTextArea textArea;
+	JScrollPane scrollText;
+	
+	public JPanel menuList()
+	{					
+		plaintext = new JButton("File Ciphertext");
+		plaintext.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				//setTableContent();
+			}
+		});
+		
+		key = new JButton("File Key");
+		key.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				//setTableContent();
+			}
+		});
+		
+		encrypt = new JButton("Mulai Dekripsi");
+		encrypt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				//setTableContent();
+			}
+		});
+		
+		save = new JButton("Simpan Hasil");
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed (ActionEvent e)
+			{
+				//setTableContent();
+			}
+		});
+		
+		pathPlain = new JTextField(2);
+		pathKey = new JTextField(2);
+		pathPlain.setEditable(false);
+		pathKey.setEditable(false);
+		
+		textArea = new JTextArea(8, 22);
+        textArea.setBorder(BorderFactory.createLineBorder(Color.black));
+        textArea.setEditable(false);
+		
+		scrollText = new JScrollPane(textArea);
+		
+		JLabel text = new JLabel("Pilih File: ");
+		menu = new JPanel();
+		layout = new GroupLayout(menu);
+		menu.setLayout(layout);		
+		layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+		layout.setHorizontalGroup(
+			layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addComponent(textArea)
+					.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+							.addComponent(text))
+						.addComponent(pathPlain)
+						.addComponent(pathKey))
+					.addComponent(encrypt, GroupLayout.Alignment.CENTER))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+					.addComponent(plaintext, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, 120)
+					.addComponent(key, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, 120)
+					.addComponent(save))
+		);
+		layout.setVerticalGroup(
+			layout.createSequentialGroup()
+				.addComponent(text)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent(pathPlain)
+					.addComponent(plaintext))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent(pathKey)
+					.addComponent(key))
+				.addComponent(encrypt)
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent(textArea)
+					.addComponent(save, GroupLayout.Alignment.CENTER))
+		);
+		return menu;
+	}
+}
---
+++ src/rc4kripto/RC4GUI.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rc4kripto;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.border.EmptyBorder;
+
+/**
+ *
+ * @author Aradea
+ */
+public class RC4GUI extends JFrame {
+	
+	private static final int FRAME_WIDTH = 520;
+    private static final int FRAME_HEIGHT = 400;
+    private static final int BORDER_WIDTH = 15;
+	
+	public RC4GUI(String title)
+    {
+        super(title);
+    }
+	
+	public void initGUI()
+    {
+        //Mengatur ukuran JFrame agar tidak dapat diubah
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        
+        //Mengubah layout JFrame menjadi BorderLayout
+        setLayout(new BorderLayout());
+        
+        //Memanggil method createTab yang akan mengembalikan
+        //JTabbedPane dan dimasukkan dalam JFrame di posisi tengah
+        add(createTab(), BorderLayout.CENTER);
+        
+        //Memindahkan posisi frame ke tengah-tengah layar
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+	private JTabbedPane createTab()
+    {
+        //Menginisialisasi sebuah objek JTabbedPane bernama tab
+        JTabbedPane tab = new JTabbedPane();
+        //Menambahkan tiga buah tab ke dalam tab
+        tab.addTab("Enkripsi", null, new EnkripGUI().menuList(), "Enkripsi RC4");
+        tab.addTab("Dekripsi", null, new DekripGUI().menuList(), "Dekripsi RC4");
+        tab.addTab("About", null, about(), "Tentang Pembuat");
+        
+        //Membuat border kosong di sekeliling tab
+        tab.setBorder(new EmptyBorder(BORDER_WIDTH, BORDER_WIDTH, BORDER_WIDTH, BORDER_WIDTH));
+        return tab;
+    }
+	
+	public static void main(String[] args) {
+		new RC4GUI("Tugas Pemrograman CIS").initGUI();
+	}
+	
+	public JPanel about() {
+		String about = "<html>Tugas Pemrograman Kriptografi & Keamanan Informasi<br/><br/>";
+		about += "Oleh:<br/>";
+		about += "Aradea Krisnaraga - 1106003900<br/>";
+		about += "Daniel Januar - 1106008403</html>";
+		JLabel tentang = new JLabel();
+		tentang.setPreferredSize(new Dimension(615, 140));
+        tentang.setText(about);
+        tentang.setHorizontalAlignment(JLabel.CENTER);
+        tentang.setVerticalAlignment(JLabel.CENTER);
+		JPanel jpanel = new JPanel();
+		jpanel.add(tentang);
+		return jpanel;
+	}
+}
